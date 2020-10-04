e1cf87b45c41249623d73e646c09c6ec37021117
Carlo Alberto Ferraris <cafxx@strayorange.com>
2020-10-04 21:43:35 +0000

    remove -regex

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rm -rf gen && mkdir -p gen
-go run main.go -regex '(?i)[[:space:]](([a-z0-9._%+-]+)@([a-z0-9.-]+\.[a-z]{2,}))[[:space:]]' > gen/main.go 
+go run main.go '(?i)[[:space:]](([a-z0-9._%+-]+)@([a-z0-9.-]+\.[a-z]{2,}))[[:space:]]' > gen/main.go 
 go build -o gen/main gen/main.go && go tool objdump -S -s main.Match gen/main > gen/main.asm
 go run gen/main.go ' dod oifoejf@fewj.coc eoj fepowk@kfoooooofsdfjdsfkdskf.com'
