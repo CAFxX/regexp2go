38bc5bc17311fe109789caf0464e4434598ef6e0
Carlo Alberto Ferraris <cafxx@mercari.com>
2021-06-11 09:43:31 +0900

    allow to override the go command

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+GOCMD=${GOCMD:-go}
+
 function build() {
     pkg=$1
     regex=$2
@@ -9,14 +11,14 @@ function build() {
 
     dir="gen/$pkg"
     mkdir -p "$dir"
-    go run main.go "$regex" > "$dir/main.go"
-    go build -o "$dir/main" "$dir/main.go"
-    go tool objdump -S -s "main.Match" "$dir/main" > "$dir/main.asm"
+    $GOCMD run main.go "$regex" > "$dir/main.go"
+    $GOCMD build -o "$dir/main" "$dir/main.go"
+    $GOCMD tool objdump -S -s "main.Match" "$dir/main" > "$dir/main.asm"
     "$dir/main" "$str"
 
     dir="examples/$pkg"
     mkdir -p "$dir"
-    go run main.go -pkg "$pkg" "$regex" > "$dir/main.go"
+    $GOCMD run main.go -pkg "$pkg" "$regex" > "$dir/main.go"
     rm -f "$dir/main"
 }
 
@@ -39,8 +41,8 @@ build suffix '[a-z]+$' 'ohvrun cbab cek aifrba  afur483hf, wjfbhjrbr ej frjebhrb
 build ipv6 '(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9]))' 'This is an IPv4: 127.1, whereas this is an IPv6: ::1.'
 
 echo "Gathering profiles"
-go test -c ./benchmark # build benchmark.test
-go test -bench=. -cpuprofile=cpu.prof -memprofile=mem.prof -benchtime=1s -cpu=1 -short ./benchmark/ >/dev/null
+$GOCMD test -c ./benchmark # build benchmark.test
+$GOCMD test -bench=. -cpuprofile=cpu.prof -memprofile=mem.prof -benchtime=1s -cpu=1 -short ./benchmark/ >/dev/null
 
 echo "Running benchmarks"
-benchstat <(go test -bench=. -benchtime=0.1s -benchmem -count=10 -cpu=1 ./benchmark/)
+benchstat <($GOCMD test -bench=. -benchtime=0.1s -benchmem -count=10 -cpu=1 ./benchmark/)
