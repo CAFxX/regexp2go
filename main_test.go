7288a910d796e5b642aa96ee7d07053c8f8b86a2
Carlo Alberto Ferraris <cafxx@mercari.com>
2021-06-11 14:16:02 +0900

    move the tests to go

---
+++ main_test.go
@@ -0,0 +1,77 @@
+package main_test
+
+import (
+	"reflect"
+	"regexp"
+	"strconv"
+	"testing"
+
+	"github.com/CAFxX/regexp2go/examples/dna"
+	"github.com/CAFxX/regexp2go/examples/ipv6"
+	"github.com/CAFxX/regexp2go/examples/line_prefix"
+	"github.com/CAFxX/regexp2go/examples/log_parse"
+	"github.com/CAFxX/regexp2go/examples/mail_crawler"
+	"github.com/CAFxX/regexp2go/examples/prefix"
+	"github.com/CAFxX/regexp2go/examples/suffix"
+	"github.com/CAFxX/regexp2go/examples/unicode"
+)
+
+func TestRegexp2go(t *testing.T) {
+	cases := map[string]struct {
+		re string
+		fn interface{} // match function
+		in []string
+	}{
+		"dna":          {dna.MatchRegexp, dna.Match, []string{"cgtgctatcggcatccaatctatccaacttctaactaccggaggactgtttttcggcaagtgcctgacttcagctgaccacatccacaggataacctggtaaactctcctctgaacagcagacacacccgaccaactatatccaggaatcttcaggcgcgcctagacacagtgatagcagggtggttgccaccgtcgtgttaaagagaaatgacatccctacttgcattacggctgttttccagccagtacaaccaggctaggagctgcaggaccgatgagggactgatggatgggtgtagcagactttcccaattgtcgagcagcctccgaccaggaccccgcaaccgcagtcctcagagcaaggctaatttaggacgctgccaactatactacattaacattttatgaaaccacggctttggatctcgccgctcgactaacggtgctctcatcccttaaggcttggacacagcagattaagtctgagacttgtgaagtgtcgatttcccgagcgatgtaagatcgcaatagtcgggcagttgccattgagtctactcgcaggacccggcagccgtcttggaacaataccgatggtacgaacgcctctaggcctcccagaaggtagcttttctccgagctcgggacatgttaacgtaatctaagggcgatagtatacacctagtagagcgcagatttattgggtccttccagaaagtatccagcgtagtagatgccaattgcaactatatggaacaagcgccacgtagtattatcagtagtagatgcccgaactgatgcttaactcgctgttcggttcgccggctcccctcgctaccacgccctctagctcccgctgggactggactcttgctagggaagtggtcacccatgggactcagatcctgtctctgcaatgtgaaaatgtatgcttagaacgcaagttgacacacgcgacgctcgagatattgagttactaagacgtttacgacgcaccttccaacttcatatccgggagggaaaaa"}},
+		"ipv6":         {ipv6.MatchRegexp, ipv6.Match, []string{"This is an IPv4: 127.1, whereas this is an IPv6: ::1."}},
+		"prefix":       {prefix.MatchRegexp, prefix.Match, []string{"> Hello world!"}},
+		"suffix":       {suffix.MatchRegexp, suffix.Match, []string{"ohvrun cbab cek aifrba  afur483hf, wjfbhjrbr ej frjebhrbiebfr frebfrvej rekhbrsfr fbrkhvbrkesf rf rbjkefbhrhfbhbrhebbrbfr jgrhh"}},
+		"log_parse":    {log_parse.MatchRegexp, log_parse.Match, []string{"ERR x\nINFO msg=hello world\nINFO res=42 msg=oh crap\nERR yadda"}},
+		"unicode":      {unicode.MatchRegexp, unicode.Match, []string{"それはちょっと。。。私は忙しいです！"}},
+		"line_prefix":  {line_prefix.MatchRegexp, line_prefix.Match, []string{"example\n>hello\n>world!\nend\n"}},
+		"mail_crawler": {mail_crawler.MatchRegexp, mail_crawler.Match, []string{" dod oifoejf@fewj.coc eoj fepowk@kfoooooofsdfjdsfkdskf.com"}},
+	}
+	for n, c := range cases {
+		c := c
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+			f := reflect.ValueOf(c.fn)
+			re := regexp.MustCompile(c.re)
+			for i, s := range c.in {
+				s := s
+				t.Run(strconv.Itoa(i), func(t *testing.T) {
+					t.Parallel()
+					t.Logf("regexp: %q", c.re)
+					t.Logf("input: %q", s)
+
+					res := f.Call([]reflect.Value{reflect.ValueOf(s)})
+					matches := make([]string, res[0].Len())
+					_m := reflect.ValueOf(matches)
+					reflect.Copy(_m, res[0])
+					pos := res[1].Int()
+					ok := res[2].Bool()
+
+					rematches := re.FindStringSubmatch(s)
+					if !reflect.DeepEqual(matches, rematches) {
+						t.Errorf("matches: got %v, expected %v", matches, rematches)
+					}
+
+					reok := re.MatchString(s)
+					if ok != reok {
+						t.Errorf("ok: got %v, expected %v", ok, reok)
+					}
+
+					_ = pos
+					/* FIXME: pos is incorrect
+					if ok {
+						repos := re.FindStringIndex(s)
+						if pos != int64(repos[0]) {
+							t.Errorf("pos: got %v, expected %v", pos, repos[0])
+						}
+					}
+					*/
+				})
+			}
+		})
+	}
+}
--- test.sh
+++ test.sh
@@ -5,7 +5,6 @@ GOCMD=${GOCMD:-go}
 function build() {
     pkg=$1
     regex=$2
-    str=$3
 
     echo "$pkg"
 
@@ -14,7 +13,6 @@ function build() {
     $GOCMD run main.go "$regex" > "$dir/main.go"
     $GOCMD build -o "$dir/main" "$dir/main.go"
     $GOCMD tool objdump -S -s "main.Match" "$dir/main" > "$dir/main.asm"
-    "$dir/main" "$str"
 
     dir="examples/$pkg"
     mkdir -p "$dir"
@@ -24,21 +22,18 @@ function build() {
 
 rm -rf gen examples benchmark.test cpu.prof mem.prof
 
-build mail_crawler '(?i)\b([a-z0-9._%+-]+)@([a-z0-9.-]+\.[a-z]{2,})\b' ' dod oifoejf@fewj.coc eoj fepowk@kfoooooofsdfjdsfkdskf.com'
-build log_parse '(?m)^INFO res=([0-9]+) msg=(.*)$' "ERR x
-INFO msg=hello world
-INFO res=42 msg=oh crap
-ERR yadda"
-build line_prefix '(?m)^>(.*)$' "example
->hello
->world!
-end
-"
-build dna '(?:(agggtaaa|tttaccct)|([cgt]gggtaaa|tttaccc[acg])|(a[act]ggtaaa|tttacc[agt]t)|(ag[act]gtaaa|tttac[agt]ct)|(agg[act]taaa|ttta[agt]cct)|(aggg[acg]aaa|ttt[cgt]ccct)|(agggt[cgt]aa|tt[acg]accct)|(agggta[cgt]a|t[acg]taccct)|(agggtaa[cgt]|[acg]ttaccct))' 'cgtgctatcggcatccaatctatccaacttctaactaccggaggactgtttttcggcaagtgcctgacttcagctgaccacatccacaggataacctggtaaactctcctctgaacagcagacacacccgaccaactatatccaggaatcttcaggcgcgcctagacacagtgatagcagggtggttgccaccgtcgtgttaaagagaaatgacatccctacttgcattacggctgttttccagccagtacaaccaggctaggagctgcaggaccgatgagggactgatggatgggtgtagcagactttcccaattgtcgagcagcctccgaccaggaccccgcaaccgcagtcctcagagcaaggctaatttaggacgctgccaactatactacattaacattttatgaaaccacggctttggatctcgccgctcgactaacggtgctctcatcccttaaggcttggacacagcagattaagtctgagacttgtgaagtgtcgatttcccgagcgatgtaagatcgcaatagtcgggcagttgccattgagtctactcgcaggacccggcagccgtcttggaacaataccgatggtacgaacgcctctaggcctcccagaaggtagcttttctccgagctcgggacatgttaacgtaatctaagggcgatagtatacacctagtagagcgcagatttattgggtccttccagaaagtatccagcgtagtagatgccaattgcaactatatggaacaagcgccacgtagtattatcagtagtagatgcccgaactgatgcttaactcgctgttcggttcgccggctcccctcgctaccacgccctctagctcccgctgggactggactcttgctagggaagtggtcacccatgggactcagatcctgtctctgcaatgtgaaaatgtatgcttagaacgcaagttgacacacgcgacgctcgagatattgagttactaagacgtttacgacgcaccttccaacttcatatccgggagggaaaaa'
-build prefix 'Hello ([^!]+)!' '> Hello world!'
-build unicode '私は((?:\p{Katakana}|\p{Hiragana}|\p{Han})+)です' 'それはちょっと。。。私は忙しいです！'
-build suffix '[a-z]+$' 'ohvrun cbab cek aifrba  afur483hf, wjfbhjrbr ej frjebhrbiebfr frebfrvej rekhbrsfr fbrkhvbrkesf rf rbjkefbhrhfbhbrhebbrbfr jgrhh'
-build ipv6 '(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9]))' 'This is an IPv4: 127.1, whereas this is an IPv6: ::1.'
+echo "Building sample regexps"
+build mail_crawler '(?i)\b([a-z0-9._%+-]+)@([a-z0-9.-]+\.[a-z]{2,})\b'
+build log_parse '(?m)^INFO res=([0-9]+) msg=(.*)$'
+build line_prefix '(?m)^>(.*)$'
+build dna '(?:(agggtaaa|tttaccct)|([cgt]gggtaaa|tttaccc[acg])|(a[act]ggtaaa|tttacc[agt]t)|(ag[act]gtaaa|tttac[agt]ct)|(agg[act]taaa|ttta[agt]cct)|(aggg[acg]aaa|ttt[cgt]ccct)|(agggt[cgt]aa|tt[acg]accct)|(agggta[cgt]a|t[acg]taccct)|(agggtaa[cgt]|[acg]ttaccct))'
+build prefix 'Hello ([^!]+)!'
+build unicode '私は((?:\p{Katakana}|\p{Hiragana}|\p{Han})+)です'
+build suffix '[a-z]+$'
+build ipv6 '(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'
+
+echo "Testing"
+$GOCMD test .
 
 echo "Gathering profiles"
 $GOCMD test -c ./benchmark # build benchmark.test
