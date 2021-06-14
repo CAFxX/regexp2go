fc1289ed854668c49befe8613cbfda2a7fbc5a6f
Carlo Alberto Ferraris <cafxx@mercari.com>
2021-06-14 09:52:45 +0900

    implement demo http server

--- main.go
+++ main.go
@@ -20,8 +20,19 @@ func main() {
 		flags = flag.Uint("flags", 212, "Flags to apply for regex compilation")
 		pkg   = flag.String("pkg", "main", "The name of the package to use for the generated code")
 		fn    = flag.String("fn", "Match", "The name of the function to use for the generated code")
+		srv   = flag.String("http", "", "Start the HTTP server on the specified address and port")
 	)
 	flag.Parse()
+
+	if *srv != "" {
+		err := internal.Server(*srv)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "http server: %v\n", err)
+			os.Exit(-1)
+		}
+		return
+	}
+
 	if nargs := len(flag.Args()); nargs != 1 {
 		fmt.Fprintf(os.Stderr, "expected 1 argument, got %d\n", nargs)
 		os.Exit(-1)
