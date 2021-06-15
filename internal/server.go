f12b1f28c1817d682ac78f6eecb97393802eaa86
Carlo Alberto Ferraris <cafxx@strayorange.com>
2021-06-15 22:04:45 +0000

    spruce up demo ui

--- internal/server.go
+++ internal/server.go
@@ -8,8 +8,12 @@ import (
 func Server(addr string) error {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`
+			<!DOCTYPE html>
 			<html>
 				<head>
+					<meta charset="utf-8">
+					<meta name="viewport" content="width=device-width, initial-scale=1">
+					<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">			
 					<title>regexp2go demo</title>
 				</head>
 				<body>
